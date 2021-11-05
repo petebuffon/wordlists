21e0f560e13afedb66b48c46786a64b81cacf780
Peter Buffon <pabuffon@gmail.com>
2021-11-04 17:03:58 -0700

    omdb update

--- episodes.py
+++ episodes.py
@@ -1,34 +1,38 @@
+#!/bin/python
+
 import sys
 import requests
+from requests import api
 
 try:
-    api_key = sys.argv[1]
+    apikey = sys.argv[1]
     imdb_id = sys.argv[2]
     outfile = sys.argv[3]
 except IndexError:
-    print("usage: episodes.py [api_key] [imdb_id] [outfile]")
+    print("usage: episodes.py [apikey] [imdb_id] [outfile]")
     sys.exit(0)
 
 if sys.argv[1] == "-help" or sys.argv[1] == "--help":
-    print("usage: episodes.py [api_key] [imdb_id] [outfile]")
+    print("usage: episodes.py [apikey] [imdb_id] [outfile]")
     sys.exit(0)
 
 # retrieve number of seasons
 print("fetching number of seasons")
-url = f"https://imdb-api.com/en/API/Title/{api_key}/{imdb_id}"
+url = f"https://www.omdbapi.com/?i={imdb_id}&apikey={apikey}"
 r = requests.get(url)
 payload = r.json()
-seasons = len(payload["tvSeriesInfo"]["seasons"])
+total_seasons = int(payload["totalSeasons"])
 
 # retrieve episode titles
 episode_list = []
-for i in range(1,seasons):
+for i in range(1, total_seasons + 1):
     print(f"fetching episode title for season {i}")
-    url = "https://imdb-api.com/en/API/SeasonEpisodes/k_vpc4r8s5/tt0411008/" + str(i)
-    json_payload = requests.get(url)
-    episodes = json_payload.json()
-    for episode in episodes["episodes"]:
-        episode_list.append(episode["title"])
+    season = str(i)
+    url = f"https://omdbapi.com/?i={imdb_id}&apikey={apikey}&season={season}"
+    r = requests.get(url)
+    payload = r.json()
+    for episode in payload["Episodes"]:
+        episode_list.append(episode["Title"].lower())
 
 # write episode files to disk
 print(f"writing results to {outfile}")
